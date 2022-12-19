194e450f1284f6aeea693a72d25a97075b91690d
Valeria Safonova <strobagatova@yandex.ru>
2022-12-19 17:50:59 +0300

    board done

--- client/src/components/Board/Board.jsx
+++ client/src/components/Board/Board.jsx
@@ -1,54 +1,77 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-
-
-
+import { initBoard, initTopics } from '../../store/board/creators';
+import cl from './Board.module.css';
 export default function Board() {
-  const initialBoard = [];
-  console.log("MOUNT BOARD");
-  const [board, setBoard] = useState(initialBoard);
-
+  const board = useSelector((store) => store.board.board);
+  const topics = useSelector((store) => store.board.topics);
+  const dispatch = useDispatch();
   useEffect(() => {
-    console.log('App Board');
     (async () => {
-      // setLoading(true);
-      const response = await fetch('http://localhost:3100/boardApi');
-      const data = await response.json();
-      console.log("▶ ⇛ data", data);
-      setBoard(data);
-      // setLoading(false);
-      console.log("▶ ⇛ oneBlock", board);
+      try {
+        const response = await fetch('http://localhost:3100/boardApi', {
+          method: 'GET',
+          credentials: 'include',
+        });
+        const { allBord, allTopics } = await response.json();
+        dispatch(initBoard(allBord));
+        dispatch(initTopics(allTopics));
+        console.log('topics', topics);
+      } catch (error) {
+        console.log(error);
+      }
     })();
   }, []);
 
-
-  if (board)
-    return (<div>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="board-head">
-            <div className="col-sm-6">Остановить игру</div>
-            <div className="col-sm-6">Очки</div>
-          </div>
+  return board.length && topics.length ? (
+    <div className={cl.table}>
+      {topics.map((top) => (
+        <div key={top.id} className={cl.row}>
+          <div className={cl.row_topic}>{top.topic}</div>
+          {board
+            .filter((el) => el.topic_id === top.id)
+            .map((quest) => (
+              <div key={quest.id} data-id={quest.id}>
+                {quest.score}
+              </div>
+            ))}
         </div>
-
-        <div className="quest-wrap">
-          <div className="container">
-            <div className="quest-block">
-              <div className="topic"></div>
-              <div className="rangeOne"></div>
-              <div className="range"></div>
-              <div className="rangeOne"></div>
-              <div className="rangeOne"></div>
-              <div className="rangeOne"></div>
-            </div>
-          </div>
-        </div>
-
-
-      </div>
-      <h1>
-        Board Page
-      </h1>
-    </div>);
+      ))}
+    </div>
+  ) : (
+    <div>Масиив пустой</div>
+  );
 }
+
+// <div>
+//   <div className="container-fluid">
+//     <div className="row">
+//       <div className="board-head">
+//         <div className="col-sm-6">Остановить игру</div>
+//         <div className="col-sm-6">Очки</div>
+//       </div>
+//     </div>
+//     {' '}
+//     <div className="quest-wrap">
+//       {' '}
+//       <div className="container">
+//         {' '}
+//         <div className="quest-block">
+//           <div className="topic"></div>
+//            <div className="rangeOne"></div>
+//            <div className="range"></div>
+//            <div className="rangeOne"></div>
+//            <div className="rangeOne"></div>
+//            <div className="rangeOne"></div>
+//           {' '}
+//         </div>
+//         {' '}
+//       </div>
+//       {' '}
+//     </div>
+//     {' '}
+//   </div>
+//    <h1>Board Page</h1>
+//   {' '}
+// </div>;
