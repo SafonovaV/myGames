2e70a74eb2792a254224a649aa7ad7ea571ad5ed
Igor.K <igor.k.nsk@gmail.com>
2022-12-20 01:02:04 +0700

    work in css in Board

--- client/src/components/Board/Board.jsx
+++ client/src/components/Board/Board.jsx
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from 'react-redux';
 
 import { initBoard, initTopics } from '../../store/board/creators';
 import cl from './Board.module.css';
+import { Button, InputGroup } from 'react-bootstrap';
+
 export default function Board() {
   const board = useSelector((store) => store.board.board);
   const topics = useSelector((store) => store.board.topics);
@@ -26,28 +28,39 @@ export default function Board() {
 
   return (
     <>
-      <div>
-        <div>Остановить игру</div>
-        <div>Очков: 2100</div>
+      <div className={cl.boardContent}>
+        <div className={cl.boardHead}>
+          <Button className={cl.bhBlock} type="button" variant="primary">Остановить игру</Button>{' '}
+          <InputGroup.Text className={cl.bhBlock} id="inputGroup-sizing-default">
+            Очков: {'200'}
+          </InputGroup.Text>
+          {/* <input defaultValue="Очков: 2100" className={cl.bhBlock} /> */}
       </div>
       {board.length && topics.length ? (
+          <div className={cl.tableWrap}>
         <div className={cl.table}>
           {topics.map((top) => (
-            <div key={top.id} className={cl.row}>
+            <div key={top.id} className={cl.tableRow}>
               <div className={cl.row_topic}>{top.topic}</div>
               {board
                 .filter((el) => el.topic_id === top.id)
                 .map((quest) => (
+                  <div className={cl.scoreBlock}>
                   <div key={quest.id} data-id={quest.id}>
                     {quest.score}
+                  </div>
+
                   </div>
                 ))}
             </div>
           ))}
         </div>
+          </div>
+
       ) : (
         <div>Масиив пустой</div>
       )}
+      </div>
     </>
   );
 }
