bb03884f87359791c0aec286b2ea5890ee1475b9
Igor.K <igor.k.nsk@gmail.com>
2022-12-20 01:14:48 +0700

    add some css in board

---
+++ client/src/components/Timer/Timer.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+
+export default function Timer() {
+
+  // useEffect(() => {
+  //   (async () => {
+  //     try {
+  //       const response = await fetch('http://localhost:3100/boardApi', {
+  //         method: 'GET',
+  //         credentials: 'include',
+  //       });
+  //       const { allBord, allTopics } = await response.json();
+
+  //       console.log('topics', topics);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   })();
+  // }, []);
+
+  return (
+    <>
+
+    </>
+  );
+}
